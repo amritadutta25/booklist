bce0307528d6a784ac27300d5e3cedec10f8a65c
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-20 21:24:07 -0500

    Added INDEX, NEW, CREATE routes and their views

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const Book = require("./models/Book.js")
 /**
  * Middleware
  */
+
 // middleware tells express to use stuff mentioned with 'app.use' for every request. app.use takes in req, res and next and 'uses' the function inside it for every request like use console.log("this is middleware"), use morgan, use express.urlencoded
 // app.use((req, res, next) => {
 //     console.log("this is middleware")
@@ -32,23 +33,40 @@ app.use(express.urlencoded({ extended: true })) // body parser, this is how we g
  */
 
 // INDEX route - GET to render all books
+app.get("/books", async (req, res) => {
+    // find all books in mongodb
+    let books = await Book.find({})
+    // render all the books to index.ejs
+    res.render("index.ejs", {
+        books: books.reverse()  // display latest added book at the top
+    })
+})
 
 // NEW route - GET for the form to create a new book
+app.get("/books/new", (req, res) => {
+    // render the create form
+    res.render("new.ejs")
+})
 
 // CREATE route - POST
 app.post("/books", async (req, res) => {
 
-    // checkbox in frontend returns 'on' if checkbox is ticked
-    if (req.body.completed === "on") { 
-        // if it is checked
-        req.body.completed = true
-    } else {
-        // if not checked
-        req.body.completed = false
-    }
+    try {
+         // checkbox in frontend returns 'on' if checkbox is ticked
+        if (req.body.completed === "on") { 
+            // if it is checked
+            req.body.completed = true
+        } else {
+            // if not checked
+            req.body.completed = false
+        }
 
-    let newBook = await Book.create(req.body)
-    res.send(newBook)
+        let newBook = await Book.create(req.body)
+        res.redirect("/books")  // send user back to index page after adding a new book
+
+    } catch (err) {
+        res.send(err)
+    }
 })
 
 // SHOW route - GET to render only one book
