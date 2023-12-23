4242a76fb47c8d3205b031b9d9f1a3daf9247866
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-23 14:01:43 -0500

    refactoring

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ require("./config/db.js") // brings in our db config. This brings in all the dep
 const express = require("express")
 const morgan = require("morgan") //logger
 const methodOverride = require("method-override")
+const booksRouter = require("./routes/books.js")
 
 const app = express()
 const { PORT = 3013 } = process.env // same as -> const PORT = process.env.PORT || 3013; // use env.PORT if it returns real value else use 3013
 
 // Bring in our model
 const Book = require("./models/Book.js")
-
+const seedData = require("./models/seed.js")
 
 
 
@@ -25,6 +26,18 @@ const Book = require("./models/Book.js")
 //     console.log("this is middleware")
 //     next() // go to next middleware
 // })
+
+// for every route, on the request object (req), add a custom object that will give access to Book model, seedData model
+// this lets us access the two models in ./routes/books.js
+app.use((req, res, next) => {
+    req.model = {
+        Book,
+        seedData
+    }
+    console.log("this is middleware")
+    next()
+})
+
 app.use(morgan("dev")) // logging
 app.use(express.urlencoded({ extended: true })) // body parser, this is how we get access to req.body
 app.use(methodOverride("_method")) // Lets us use DELETE PUT HTTP verbs 
@@ -33,107 +46,9 @@ app.use(methodOverride("_method")) // Lets us use DELETE PUT HTTP verbs
  * Routes and Router
  * INDUCES
  */
-
-// INDEX route - GET to render all books
-app.get("/books", async (req, res) => {
-    // find all books in mongodb
-    let books = await Book.find({})
-    // render all the books to index.ejs
-    res.render("index.ejs", {
-        books: books.reverse()  // display latest added book at the top
-    })
-})
-
-// NEW route - GET for the form to create a new book
-app.get("/books/new", (req, res) => {
-    // render the create form
-    res.render("new.ejs")
-})
-
-// DELETE
-app.delete("/books/:id", async (req, res) => {
-    try {
-        // Find a book and then delete
-        let deletedBook = await Book.findByIdAndDelete(req.params.id)
-        // redirect back to the index
-        res.redirect("/books")
-        
-    } catch (error) {
-        res.status(500).send("something went wrong when deleting")
-    }
-})
-
-// UPDATE route
-app.put("/books/:id", async (req, res) => {
-    try {
-        // handle the checkbox logic
-
-    // ticked 'checkbox' from front end returns 'on', but based on our model schema 'completed' expects a boolean value so we reformat the 'completed' field value
-    if (req.body.completed === "on") {
-        req.body.completed = true
-    } else {
-        req.body.completed = false
-    }
-
-    // then find the book by id and update with the req.body
-    //findByIdUpdate(id, data to update, options)
-    let updatedBook = await Book.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-            new: true
-        }
-    )
-
-    // redirect to show route with the updated book
-    res.redirect(`/books/${updatedBook._id}`)
-    } catch (error) {
-        res.send("something went wrong in this route")
-    }
-})
-
-// CREATE route - POST
-app.post("/books", async (req, res) => {
-
-    try {
-         // checkbox in frontend returns 'on' if checkbox is ticked, but based on our model schema 'completed' expects a boolean value so we reformat the 'completed' field value
-        if (req.body.completed === "on") { 
-            // if it is checked
-            req.body.completed = true
-        } else {
-            // if not checked
-            req.body.completed = false
-        }
-
-        let newBook = await Book.create(req.body)
-        res.redirect("/books")  // send user back to index page after adding a new book
-
-    } catch (err) {
-        res.send(err)
-    }
-})
-
-//EDIT route - get the book to edit
-app.get("/books/edit/:id", async (req, res) => {
-    try {
-        // find the book to edit
-        let foundBook = await Book.findByIdAndUpdate(req.params.id)
-        res.render("edit.ejs", {
-            book: foundBook
-        })
-    } catch (error) {
-        res.send(error)
-    }
-})
-
-// SHOW route - GET to render only one book
-app.get("/books/:id", async (req, res) => {
-    // find a book by the '_id'
-    let foundBook = await Book.findById(req.params.id)
-
-    // render the book to show.ejs
-    res.render("show.ejs", {book: foundBook})
-})
+// app.use(prefix url, router to execute)
+// tells server to start every route with "/books" followed by the specific routes present in  each route in "./routes/book.js"
+app.use("/books", booksRouter)
 
 /**
  * Server listener
